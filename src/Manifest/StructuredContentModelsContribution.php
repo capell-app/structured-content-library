1f2cc0918db97a5c506f118cc4355fd135d49f24
Ben <howdu@users.noreply.github.com>
2026-07-12 07:44:22 +0100

    fix: align packages with 0.0 release line

--- src/Manifest/StructuredContentModelsContribution.php
+++ src/Manifest/StructuredContentModelsContribution.php
@@ -10,6 +10,6 @@ final class StructuredContentModelsContribution implements ExtensionContribution
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^0.0';
     }
 }
